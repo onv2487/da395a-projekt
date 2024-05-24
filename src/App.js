943891dee8e030b6d3304f4b654ba45ace5823e9
StefanYousif <102787518+StefanYousif@users.noreply.github.com>
2024-05-24 11:21:29 +0200

    router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
 import SavedRecipes from './pages/SavedRecipes';
@@ -8,18 +8,29 @@ import './App.css';
 const App = () => {
 
     return (
+        <Router>
+        <Header />
+        <SearchBar />
 
+        <Routes>
 
+            <Route path="/" element={<Home />} />
+            <Route path="/saved" element={<SavedRecipes />} />
+        </Routes>
+        </Router>
+
+    /*
         <Router>
             <Header />
             <Routes>
 
                 <Route path="/" element={<Home />} />
                 <Route path="/saved" element={<SavedRecipes />} />
-                {/* Error route */}
+                {/* Error route }
                 <Route path="*" element={<NotFound />} />
             </Routes>
         </Router>
+    */
     );
 };
 
