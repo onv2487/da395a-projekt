44c8a35dd9dae9f154fcfc227808d179305f5eea
StefanYousif <102787518+StefanYousif@users.noreply.github.com>
2024-05-24 14:19:06 +0200

    ..

--- src/components/RecipeCard/RecipeCard.js
+++ src/components/RecipeCard/RecipeCard.js
@@ -3,8 +3,9 @@ import './RecipeCard.css';
 
 const RecipeCard = ({ recipe, onSave}) => (
     <div className="recipe-card">
-        <h2>{recipe.tite}</h2>
+        <h2>{recipe.title}</h2>
         <img src={recipe.image} alt={recipe.title} />
+        <p>{recipe.description}</p>
         <button onClick= {() => onSave(recipe)}>Spara</button>
     </div>
 );
