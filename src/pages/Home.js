6a479d29aa4939ab64ca4e09c3d1172c11a57b33
StefanYousif <102787518+StefanYousif@users.noreply.github.com>
2024-05-24 13:41:09 +0200

    api key

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
     const [recipes, setRecipes] = useState([]);
 
     const handleSearch = (query) => {
-        axios.get(`http://www.omdbapi.com/?apikey=870bfxxx&s=${encodeURIComponent(query.trim())}`)
+        axios.get(`http://www.omdbapi.com/?apikey=870bf5be&s=${encodeURIComponent(query.trim())}`)
         //(`https://api.spoonacular.com/recipes/complexSearch?query=${query}&apiKey=YOUR_API_KEY`)
         .then(response => {
             setRecipes(response.data.results);
