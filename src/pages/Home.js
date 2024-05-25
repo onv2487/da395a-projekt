f519b41350dae4460a62f237184ba3b34c33129c
StefanYousif <102787518+StefanYousif@users.noreply.github.com>
2024-05-25 13:49:59 +0200

    fetch

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import axios from 'axios';
 import SearchBar from "../components/SearchBar/SearchBar";
 import RecipeCard from "../components/RecipeCard/RecipeCard";
@@ -7,6 +7,26 @@ import "./Home.css";
 const Home = () => {
     const [recipes, setRecipes] = useState([]);
 
+    useEffect (() => {
+        fetch(`https://api.spoonacular.com/recipes/complexSearch?query=${query}&apiKey=4f5c4482685449dbb9a7d54b3c97b2e5`)
+        .then(respone => respone.json())
+        .then(data => {
+            //hämta all nödvändig data
+            const formattedRecips = data.map(recipe => ({
+                title: recipe.title,
+                image: recipe.image,
+                description: recipe.description,
+                prepTime: recipe.prepTime,
+                cookTime: recipe.cookTime,
+                ingredients: recipe.ingredients,
+            }));
+            setRecipes(formattedRecips);
+        })
+        .catch(error =>
+            console.error('Error fetching recipes:', error));
+        
+    }, []);
+    /*
     const handleSearch = (query) => {
         axios.get(`https://api.spoonacular.com/recipes/complexSearch?query=${query}&apiKey=4f5c4482685449dbb9a7d54b3c97b2e5`)
         .then(response => {
@@ -23,6 +43,7 @@ const Home = () => {
             console.error('Error fetching data:', error);
         });
     };
+    */
     
     const handleSave = (recipe) => {
         let savedRecipes = JSON.parse(localStorage.getItem("savedRecipes")) || {};
@@ -35,15 +56,23 @@ const Home = () => {
     };
     
     return (
-        <div className="home">
-            <SearchBar onSearch={handleSearch} />
-            <div className="recipes-container">
-                {recipes.map(recipe => (
-                    <RecipeCard key={recipe.id} recipe={recipe} onSave={handleSave} />
-                ))}
-                {recipes.length === 0 && <p className="no-results">Inga recept hittades. Försök igen med ett annat sökord.</p>}
-            </div>
+
+        <div>
+            {recipes.map((recipe, index) => (
+            <RecipeCard key={index} recipe={recipe} onSave={(savedRecipe) => console.log('Saved recipe:', savedRecipe)} />
+            ))}
         </div>
+        /*
+            <div className="home">
+                <SearchBar onSearch={handleSearch} />
+                <div className="recipes-container">
+                    {recipes.map(recipe => (
+                        <RecipeCard key={recipe.id} recipe={recipe} onSave={handleSave} />
+                    ))}
+                    {recipes.length === 0 && <p className="no-results">Inga recept hittades. Försök igen med ett annat sökord.</p>}
+                </div>
+            </div>
+        */
     );
 };
 
