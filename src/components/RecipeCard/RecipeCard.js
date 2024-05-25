d3f0ad0dfb46138ab63ad9c4f42df6e3ef431cbf
StefanYousif <102787518+StefanYousif@users.noreply.github.com>
2024-05-25 17:16:35 +0200

    lista för ingredienser

--- src/components/RecipeCard/RecipeCard.js
+++ src/components/RecipeCard/RecipeCard.js
@@ -6,11 +6,17 @@ const RecipeCard = ({ recipe, onSave}) => (
         <img src={recipe.image} alt={recipe.title} />
         <div className="recipe-content">
             <h2 className="recipe-title">{recipe.title}</h2>
-            <p className="recipe-description">{recipe.description}</p>
+            <p className="recipe-description" dangerouslySetInnerHTML={{ __html: recipe.description }}>{recipe.description}</p>
             <div className="recipe-details">
                 <p><strong>Förberedelse:</strong> {recipe.prepTime} minuter</p>
                 <p><strong>Matlagning:</strong> {recipe.cookTime} minuter</p>
-                <p><strong>Ingredienser:</strong> {recipe.ingredients ? recipe.ingredients.join(', ') : 'Ingen information'}</p>
+                <p><strong>Ingredienser:</strong></p>
+                <ul>
+                    {recipe.ingredients ? recipe.ingredients.map((ingredient, index) => (
+                        <li key={index}>{ingredient}</li>
+                    )) : <li>Ingen information</li>}
+                </ul>
+
             </div>
             <button onClick= {() => onSave(recipe)}>Spara</button>
         </div> 
