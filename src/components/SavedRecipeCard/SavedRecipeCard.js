d37836db95db62930c69061bfe7d7e9f0a594840
StefanYousif <102787518+StefanYousif@users.noreply.github.com>
2024-05-26 13:43:38 +0200

    css

--- src/components/SavedRecipeCard/SavedRecipeCard.js
+++ src/components/SavedRecipeCard/SavedRecipeCard.js
@@ -2,12 +2,12 @@ import React from 'react';
 import './SavedRecipeCard.css';
 
 const SavedRecipeCard = ({ recipe, onDelete}) => (
-    <div className="recipe-card">
+    <div className="saved-recipe-card">
         <img src={recipe.image} alt={recipe.title} />
-        <div className="recipe-content">
+        <div className="saved-recipe-content">
 
-            <div className="recipe-details">
-                <h2 className="recipe-title">{recipe.title}</h2>
+            <div className="saved-recipe-details">
+                <h2 className="saved-recipe-title">{recipe.title}</h2>
                 <p><strong>Förberedelse:</strong> {recipe.prepTime} minuter</p>
                 <p><strong>Matlagning:</strong> {recipe.cookTime} minuter</p>
                 <p><strong>Ingredienser:</strong></p>
