f34fb2e5701a4c66cee6de4593b522a883473a10
StefanYousif <102787518+StefanYousif@users.noreply.github.com>
2024-05-28 17:46:20 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import React, {useState} from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
-import SearchBar from './components/SearchBar/SearchBar';
 import Home from './pages/Home';
 import SavedRecipes from './pages/SavedRecipes';
 import './App.css';
 
 const App = () => {
-
-    const[searchQuery, setSearchQuery] = useState('');
-
-    const handleSearch = (query) => {
-        setSearchQuery(query);
+    const handleSearch = (searchQuery) => {
+        setQuery(searchQuery);
     };
-    
+
     return (
         <Router>
             <Header onSearch={handleSearch} />
-            <SearchBar searchQuery={searchQuery} onSearch={handleSearch} />
 
             <Routes>
 
