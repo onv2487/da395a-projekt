c65789f34b951457a7823b09aa7f951fcd3fd990
StefanYousif <102787518+StefanYousif@users.noreply.github.com>
2024-05-28 13:33:51 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
     return (
         <Router>
             <Header onSearch={handleSearch} />
-            <SearchBar searchQuery={searchQuery} onSearch={handleSearch}
+            <SearchBar searchQuery={searchQuery} onSearch={handleSearch} />
 
             <Routes>
 
